5156b8dacc0eb8271f75441cf4e54d2d033831a7
Joshua Sells <joshuataylorsells@gmail.com>
2021-04-19 16:15:25 -0500

    message

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,16 +2,16 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Joshua Sells / JS Developer', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'Welcome to my portfolio website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Joshua Sells',
+  subtitle: 'I am a JavaScript Developer',
   cta: '',
 };
 
