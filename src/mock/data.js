16db7bb87ba62e1c9a88be1607ca39f6debd9a3f
Joshua Sells <joshuataylorsells@gmail.com>
2021-04-27 13:55:25 -0500

    Fixes a style bug, edits some data.

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,13 +18,13 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'professionalPhoto.jpg',
-  paragraphOne: 'I am an aspiring JavaScript Developer. I am a husband and father. I love programming and building new things. ' +
-  'My goal is to create an application someday that will drastically help the normal person become more organized, habitual, and happy!',
+  paragraphOne: 'I am a JavaScript Developer. I am a husband and father. I love programming and building new things. ' +
+  'My goal is to create an application someday that will drastically help the normal person become more organized, habitual, and happy.',
   paragraphTwo: 'When I graduated high school in 2015, I didn' + "'" + 't know what I wanted to do with my life. I was detail oriented, systems driven, and loved to build things. ' +
   'The problem was that I was never exposed to the concept of programming. Because of this, I was unsure about my dream occupation for a few years. ' +
   'After getting married and starting a family, I started pursuing a degree in Computer Science at the University of Concordia, St. Paul.',
   paragraphThree: 'It has been a crazy past few years of learning computer science, raising my family, and working full time. I am set to graduate in August 2021 and am currently looking for job opportunities! ' +
-  'Please feel free to check out my site and reach out for a chat!',
+  'Please feel free to check out my resume or reach out!',
   resume: 'https://drive.google.com/file/d/1P2dN50VHwPntq2vYhJuqbOcjHH_TsYxo/view?usp=sharing', // if no resume, the button will not show up
 };
 
